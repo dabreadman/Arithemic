c8c68323ee4394bf605a7a0ef71426b99a3e9092
dabreadman <43485369+dabreadman@users.noreply.github.com>
2020-01-31 15:09:14 +0000

    Added skeleton files

---
+++ src/ArithTest.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+//-------------------------------------------------------------------------
+/**
+ *  Test class for Arith.java
+ *
+ *  @author  YI XIANG TAN aka dabreadman
+ *  @version 31/01/2020 15:00:26
+ */
+@RunWith(JUnit4.class)
+public class ArithTest {
+
+	//~ Public Methods ........................................................
+	
+	
+
+
+}
